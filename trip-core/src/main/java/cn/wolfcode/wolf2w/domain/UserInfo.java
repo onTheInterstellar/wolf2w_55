57254a99b73d299043bb32301992b426c0a98d18
onTheInterstellar <17806275598@163.com>
2021-08-04 15:37:58 +0800

    初始化项目

---
+++ trip-core/src/main/java/cn/wolfcode/wolf2w/domain/UserInfo.java
@@ -0,0 +1,10 @@
+package cn.wolfcode.wolf2w.domain;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serializable;
+
+@TableName("userinfo")
+public class UserInfo implements Serializable {
+
+}
