74c232ca4c6ebc9e87af8ed3685df0d60eab888e
onTheInterstellar <17806275598@163.com>
2021-08-04 20:39:27 +0800

    新增代码

---
+++ trip-website-api/src/main/java/cn/wolfcode/wolf2w/controller/UserInfoController.java
@@ -0,0 +1,12 @@
+package cn.wolfcode.wolf2w.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserInfoController {
+
+
+
+}
